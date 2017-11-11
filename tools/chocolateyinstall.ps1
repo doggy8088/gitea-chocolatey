34bfbb614341f4751c9aef054f31780f3a3e0b53
Will <doggy.huang@gmail.com>
2017-11-11 21:30:56 +0800

    Upgrade to Gitea 1.2.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'gitea'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.gitea.io/gitea/1.2.0/gitea-1.2.0-windows-4.0-386.exe'
-$url64      = 'https://dl.gitea.io/gitea/1.2.0/gitea-1.2.0-windows-4.0-amd64.exe'
+$url        = 'https://dl.gitea.io/gitea/1.2.3/gitea-1.2.3-windows-4.0-386.exe'
+$url64      = 'https://dl.gitea.io/gitea/1.2.3/gitea-1.2.3-windows-4.0-amd64.exe'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
@@ -15,9 +15,9 @@ $packageArgs = @{
 
   softwareName  = 'Gitea'
 
-  checksum      = '3b7cc6e70d8c43a82ae20b03a93b804f270f1c190b7ae05ebdd252b9b8fe6796'
+  checksum      = '72702070a3dc1aa93daa205c1dc86d994cf36e992f8024b0c7780bbdbe6aa8d3'
   checksumType  = 'sha256'
-  checksum64    = '252eeffec28ccc591982284f6a2406cf0adc30a3820d563e21ecf68832c22879'
+  checksum64    = '49490f957622319763c4b1eea78402b4ddb3c05bc384611ffc63135ae745b957'
   checksumType64= 'sha256'
 
   fileFullPath  = "$toolsDir\gitea.exe"
